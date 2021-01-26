1947aed8dc60728a3da7eebeeac7ab63f48f7e3b
Petr Ancinec <ancinpet@fit.cvut.cz>
2021-01-27 00:47:02 +0100

    Collisions now correctly count points

--- Assets/Scripts/CheckSlashing.cs
+++ Assets/Scripts/CheckSlashing.cs
@@ -7,10 +7,60 @@ public class CheckSlashing : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == gameObject.layer)
+        // Holding Blue Weapon
+        if (gameObject.name == "LeftWeapon")
         {
-            Destroy(other.gameObject);
-            HandleScore.scoreValue += 1;
+            // Correct angle hit +1 score
+            if (other.gameObject.name == "SuccessBLUE")
+            {
+                Destroy(other.gameObject.transform.parent.gameObject);
+                HandleScore.scoreValue += 1;
+            }
+            // Wrong angle hit -3 score
+            else if (other.gameObject.name == "FailBLUE")
+            {
+                Destroy(other.gameObject.transform.parent.gameObject);
+                HandleScore.scoreValue -= 3;
+            }
+            // Correct angle but wrong color -5 score
+            else if (other.gameObject.name == "SuccessRED")
+            {
+                Destroy(other.gameObject.transform.parent.gameObject);
+                HandleScore.scoreValue -= 5;
+            }
+            // Wrong angle and color -8 score
+            else if (other.gameObject.name == "FailRED")
+            {
+                Destroy(other.gameObject.transform.parent.gameObject);
+                HandleScore.scoreValue -= 8;
+            }
+        }
+        else if (gameObject.name == "RightWeapon")
+        {
+            // Correct angle hit +1 score
+            if (other.gameObject.name == "SuccessRED")
+            {
+                Destroy(other.gameObject.transform.parent.gameObject);
+                HandleScore.scoreValue += 1;
+            }
+            // Wrong angle hit -3 score
+            else if (other.gameObject.name == "FailRED")
+            {
+                Destroy(other.gameObject.transform.parent.gameObject);
+                HandleScore.scoreValue -= 3;
+            }
+            // Correct angle but wrong color -5 score
+            else if (other.gameObject.name == "SuccessBLUE")
+            {
+                Destroy(other.gameObject.transform.parent.gameObject);
+                HandleScore.scoreValue -= 5;
+            }
+            // Wrong angle and color -8 score
+            else if (other.gameObject.name == "FailBLUE")
+            {
+                Destroy(other.gameObject.transform.parent.gameObject);
+                HandleScore.scoreValue -= 8;
+            }
         }
 
         if (other.gameObject.layer == LayerMask.NameToLayer("Menu"))
@@ -20,7 +70,8 @@ public class CheckSlashing : MonoBehaviour
                 HandleScore.currentScene = 2;
                 HandleScore.scoreValue = 0;
                 SceneManager.LoadScene("AlanWalkerScene", LoadSceneMode.Additive);
-            } else if (other.gameObject.name == "LevelModeButton")
+            }
+            else if (other.gameObject.name == "LevelModeButton")
             {
 
             }
